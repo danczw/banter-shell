7f5250e276c10844b05f16b3a97a6b55cdc67364
danczw <dev.dc@pm.me>
2023-07-25 02:17:29 +0200

    add basic chat features

---
+++ src/lib.rs
@@ -0,0 +1,141 @@
+use clap::{Arg, ArgAction, ArgMatches, Command};
+use dirs::home_dir;
+use reqwest::{Client, StatusCode};
+use serde_json::{json, Map, Value};
+use std::io::Write;
+use std::path::PathBuf;
+
+pub fn set_home_dir_path(file_name: &str) -> PathBuf {
+    let mut path = home_dir().unwrap();
+    path.push(file_name);
+    path
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Context {
+    pub openai_key: String,
+    pub hist: Vec<String>,
+}
+
+pub fn read_context(hist_file_path: &PathBuf) -> Context {
+    // Initialize Context struct
+    let mut ctx = Context {
+        openai_key: String::from(""),
+        hist: vec![],
+    };
+    // Read game file
+    let saved = std::fs::read_to_string(hist_file_path).unwrap_or("".to_string());
+
+    // if file is empty or doesn't exist, delete potential file return empty Context struct
+    if saved.is_empty() {
+        std::fs::remove_file(hist_file_path).unwrap();
+        ctx
+    } else {
+        // get openai key from first line of file
+        ctx.openai_key = saved.lines().next().unwrap().to_string();
+        // get chat history from rest of file
+        for line in saved.lines().skip(1) {
+            ctx.hist.push(line.to_string());
+        }
+        ctx
+    }
+}
+
+pub fn new_context(hist_file_path: &PathBuf) -> Context {
+    // Initialize Context struct
+    let mut ctx = Context {
+        openai_key: "".to_string(),
+        hist: vec![],
+    };
+    // get openai key from user
+    println!("No OpenAI API key found, please enter:");
+    let mut openai_key = String::new();
+    std::io::stdin()
+        .read_line(&mut openai_key)
+        .expect("Failed to read line");
+    ctx.openai_key = openai_key.trim().to_string();
+
+    // write openai key to file
+    let mut file = std::fs::File::create(hist_file_path).unwrap();
+    writeln!(file, "{}", ctx.openai_key).unwrap();
+
+    ctx
+}
+
+pub fn cli() -> Command {
+    Command::new("bsh")
+        .about("A cli designed to facilitate seamless text-based conversations with ChatGPT.")
+        .arg_required_else_help(true)
+        .arg(
+            Arg::new("message")
+                .help("The message to send to ChatGPT in quotes.")
+                .short('m')
+                .long("message")
+                .action(ArgAction::Set)
+                .required(true),
+        )
+    // TODO: arg to remove local context
+}
+
+pub async fn call_oai(
+    ctx: &Context,
+    arg: &ArgMatches,
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let new_msg = arg.get_one::<String>("message").unwrap();
+
+    // Build the URL
+    let url = "https://api.openai.com/v1/chat/completions";
+
+    // Build the body
+    let mut body: Map<String, Value> = Map::new();
+    body.insert("model".to_string(), json!("gpt-4"));
+
+    let mut messages = Vec::new();
+    for ctx_msg in ctx.hist.iter() {
+        let role = ctx_msg.split("||").next().unwrap();
+        let content = ctx_msg.split("||").nth(1).unwrap();
+        messages.push(json!({"role": role, "content": content}));
+    }
+    messages.push(json!({"role": "user", "content": new_msg}));
+    body.insert("messages".to_string(), Value::Array(messages));
+
+    let body_json = Value::Object(body);
+
+    // Initialize client and send request
+    let client: Client = reqwest::Client::new();
+    let resp = client
+        .post(url)
+        .header("Content-Type", "application/json")
+        .header(
+            "Authorization",
+            "Bearer ".to_owned() + ctx.openai_key.as_str(),
+        )
+        .json(&body_json)
+        .send()
+        .await?;
+
+    check_response(resp).await
+}
+
+pub async fn check_response(resp: reqwest::Response) -> Result<Value, Box<dyn std::error::Error>> {
+    // Get response values
+    let resp_status = resp.status();
+    // Deserialize response text
+    let resp_text = resp.text().await?;
+    let resp_json: Value = serde_json::from_str(&resp_text)?;
+
+    // check response
+    match resp_status {
+        StatusCode::OK => {
+            // return response text
+            Ok(resp_json)
+        }
+        _ => {
+            // return error message
+            Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                resp_text,
+            )))
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use banter_shell as bsh;
+use colored::*;
+use std::io::Write;
+
+#[tokio::main]
+async fn main() {
+    // set path to .banterrc
+    // TODO: add logging
+    const BSH_PROFILE: &str = ".bsh_profile";
+    let bsh_profile_path = bsh::set_home_dir_path(BSH_PROFILE);
+
+    let mut ctx = if bsh_profile_path.exists() {
+        // read .banterrc if it exists to get openai key and chat history
+        let temp_ctx = bsh::read_context(&bsh_profile_path);
+        // check if openai key is empty
+        match temp_ctx.openai_key.is_empty() {
+            // if openai key is empty, prompt user for openai key
+            true => bsh::new_context(&bsh_profile_path),
+            // else return context
+            false => temp_ctx,
+        }
+    } else {
+        // create .banterrc if it doesn't exist and prompt user for openai key
+        std::fs::File::create(&bsh_profile_path).unwrap();
+        bsh::new_context(&bsh_profile_path)
+    };
+
+    // parse command line arguments
+    let matches = bsh::cli().get_matches();
+
+    // call OpenAI API and display response
+    let oai_response = bsh::call_oai(&ctx, &matches).await;
+    match oai_response {
+        Ok(resp_value) => {
+            let answer = resp_value["choices"][0]["message"]["content"]
+                .as_str()
+                .unwrap();
+            println!("{}", answer.cyan());
+            // add message and answer to chat history
+            ctx.hist
+                .push("user||".to_owned() + matches.get_one::<String>("message").unwrap().as_str());
+            ctx.hist.push("assistant||".to_owned() + answer);
+            // clear profile file and write key as well as last 6 messages to file
+            let mut file = std::fs::File::create(&bsh_profile_path).unwrap();
+            writeln!(file, "{}", ctx.openai_key).unwrap();
+            for line in ctx.hist.iter().rev().take(6).rev() {
+                writeln!(file, "{}", line.replace('\n', "")).unwrap();
+            }
+        }
+        Err(e) => println!("{}", e),
+    }
+
+    // add
+}
