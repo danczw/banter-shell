2f9fe198344c0b4b946a904ff33ac2356b46040d
Daniel <dev.dc@pm.me>
2023-07-29 23:54:24 +0200

    Rename package (#8)
    * package renaming
    * udpate README
    * remove redundant import

--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
-    use banter_shell as bsh;
     use dirs::home_dir;
+    use gtc;
     use std::fs::File;
     use std::io::{self, Write};
     use std::path::PathBuf;
@@ -11,7 +11,7 @@ mod tests {
     fn test_set_home_dir_path() {
         let file_name = "test.txt";
         let expected_path = home_dir().unwrap().join(file_name);
-        assert_eq!(bsh::set_home_dir_path(file_name), expected_path);
+        assert_eq!(gtc::set_home_dir_path(file_name), expected_path);
     }
 
     #[test]
@@ -20,7 +20,7 @@ mod tests {
         let subdir = "subdir";
         let expected_path = home_dir().unwrap().join(subdir).join(file_name);
         assert_eq!(
-            bsh::set_home_dir_path(&format!("{}/{}", subdir, file_name)),
+            gtc::set_home_dir_path(&format!("{}/{}", subdir, file_name)),
             expected_path
         );
     }
@@ -28,74 +28,74 @@ mod tests {
     // read_context tests
     #[test]
     fn test_read_context() {
-        let mut file = File::create(".bsh_profile").unwrap();
+        let mut file = File::create(".gtc_profile").unwrap();
         writeln!(file, "openai_key").unwrap();
         writeln!(file, "message 1").unwrap();
         writeln!(file, "message 2").unwrap();
         file.flush().unwrap();
 
-        let context_file_path = PathBuf::from(".bsh_profile");
-        let expected_context = bsh::Context {
+        let context_file_path = PathBuf::from(".gtc_profile");
+        let expected_context = gtc::Context {
             openai_key: "openai_key".to_string(),
             hist: vec!["message 1".to_string(), "message 2".to_string()],
         };
-        assert_eq!(bsh::read_context(&context_file_path), expected_context);
+        assert_eq!(gtc::read_context(&context_file_path), expected_context);
 
-        std::fs::remove_file(".bsh_profile").unwrap();
+        std::fs::remove_file(".gtc_profile").unwrap();
     }
 
     #[test]
     fn test_read_context_with_empty_file() {
-        let mut file = File::create(".bsh_profile").unwrap();
+        let mut file = File::create(".gtc_profile").unwrap();
         file.flush().unwrap();
 
-        let context_file_path = PathBuf::from(".bsh_profile");
-        let expected_context = bsh::Context {
+        let context_file_path = PathBuf::from(".gtc_profile");
+        let expected_context = gtc::Context {
             openai_key: "".to_string(),
             hist: vec![],
         };
-        assert_eq!(bsh::read_context(&context_file_path), expected_context);
+        assert_eq!(gtc::read_context(&context_file_path), expected_context);
     }
 
     #[test]
     #[should_panic(expected = "No such file or directory")]
     fn test_read_context_with_invalid_file() {
         let context_file_path = PathBuf::from("/invalid/path");
-        bsh::read_context(&context_file_path);
+        gtc::read_context(&context_file_path);
     }
 
     #[test]
     fn test_read_context_with_empty_key() {
-        let mut file = File::create(".bsh_profile").unwrap();
+        let mut file = File::create(".gtc_profile").unwrap();
         writeln!(file).unwrap();
         writeln!(file, "message 1").unwrap();
         writeln!(file, "message 2").unwrap();
         file.flush().unwrap();
 
-        let context_file_path = PathBuf::from(".bsh_profile");
-        let expected_context = bsh::Context {
+        let context_file_path = PathBuf::from(".gtc_profile");
+        let expected_context = gtc::Context {
             openai_key: "".to_string(),
             hist: vec!["message 1".to_string(), "message 2".to_string()],
         };
-        assert_eq!(bsh::read_context(&context_file_path), expected_context);
+        assert_eq!(gtc::read_context(&context_file_path), expected_context);
 
-        std::fs::remove_file(".bsh_profile").unwrap();
+        std::fs::remove_file(".gtc_profile").unwrap();
     }
 
     #[test]
     fn test_read_context_with_empty_history() {
-        let mut file = File::create(".bsh_profile").unwrap();
+        let mut file = File::create(".gtc_profile").unwrap();
         writeln!(file, "openai_key").unwrap();
         file.flush().unwrap();
 
-        let context_file_path = PathBuf::from(".bsh_profile");
-        let expected_context = bsh::Context {
+        let context_file_path = PathBuf::from(".gtc_profile");
+        let expected_context = gtc::Context {
             openai_key: "openai_key".to_string(),
             hist: vec![],
         };
-        assert_eq!(bsh::read_context(&context_file_path), expected_context);
+        assert_eq!(gtc::read_context(&context_file_path), expected_context);
 
-        std::fs::remove_file(".bsh_profile").unwrap();
+        std::fs::remove_file(".gtc_profile").unwrap();
     }
 
     // input tests
@@ -103,7 +103,7 @@ mod tests {
     fn test_input() {
         let mut writer = Vec::new();
         let reader = io::Cursor::new(b"yes");
-        let result = bsh::input("Does this test pass?", reader, &mut writer).unwrap();
+        let result = gtc::input("Does this test pass?", reader, &mut writer).unwrap();
         assert_eq!(result, "yes");
         assert_eq!(writer, b"Does this test pass? ");
     }
