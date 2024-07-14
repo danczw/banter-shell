4afba5d80654067b6dd3b7e6b1657492bdbd5991
danczw <dev.dc@pm.me>
2024-07-14 21:42:09 +0200

    add basic logger config

---
+++ src/logger.rs
@@ -0,0 +1,45 @@
+use log::LevelFilter;
+use log4rs::{
+    append::{
+        console::{ConsoleAppender, Target},
+        file::FileAppender,
+    },
+    config::{Appender, Config, Root},
+    encode::pattern::PatternEncoder,
+    filter::threshold::ThresholdFilter,
+};
+use std::path::PathBuf;
+
+pub fn setup_logger(log_path: PathBuf) -> Config {
+    let level = log::LevelFilter::Warn;
+
+    // Build a stderr logger.
+    let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
+
+    // Logging to log file.
+    let pattern = "{d(%Y-%m-%d %H:%M:%S %Z)(utc)} | {l} | {m}{n}";
+    let logfile = FileAppender::builder()
+        // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
+        .encoder(Box::new(PatternEncoder::new(pattern)))
+        .build(log_path)
+        .unwrap();
+
+    // Log Trace level output to file where trace is the default level
+    // and the programmatically specified level to stderr.
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .appender(
+            Appender::builder()
+                .filter(Box::new(ThresholdFilter::new(level)))
+                .build("stderr", Box::new(stderr)),
+        )
+        .build(
+            Root::builder()
+                .appender("logfile")
+                .appender("stderr")
+                .build(LevelFilter::Trace),
+        )
+        .unwrap();
+
+    config
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 use colored::*;
+use log::{debug, error, info}; // trace, warn
+mod logger;
 use std::io::{self, Write};
 
 #[tokio::main]
 async fn main() {
-    const GTC_PROFILE: &str = ".gtc";
-    let gtc_profile_path = gtc::set_home_dir_path(GTC_PROFILE);
+    let log_path = gtc::set_home_dir_path(".gtc.log");
+    let log_config = logger::setup_logger(log_path);
+    let _handle = log4rs::init_config(log_config).unwrap();
+
+    const PROFILE: &str = ".gtc";
+    let profile_path = gtc::set_home_dir_path(PROFILE);
+    debug!("profile_path - {}", profile_path.to_string_lossy());
 
     let matches = gtc::cli().get_matches();
 
     if matches.contains_id("message") {
-        let mut ctx = if gtc_profile_path.exists() {
-            let mut ctx_read = gtc::read_context(&gtc_profile_path);
+        let mut ctx = if profile_path.exists() {
+            let mut ctx_read = gtc::read_context(&profile_path);
 
             match ctx_read.openai_key.is_empty() {
                 // prompt user for openai key
                 true => {
+                    info!("No OpenAI API key found");
                     // get openai key from user
                     let openai_key = gtc::input(
                         "No OpenAI API key found, please enter:",
@@ -56,13 +64,16 @@ async fn main() {
                 ctx.hist.push("assistant||".to_owned() + answer);
 
                 // clear profile file and write key as well as last 6 messages to file
-                let mut file = std::fs::File::create(&gtc_profile_path).unwrap();
+                let mut file = std::fs::File::create(&profile_path).unwrap();
                 writeln!(file, "{}", ctx.openai_key).unwrap();
                 for line in ctx.hist.iter().rev().take(6).rev() {
                     writeln!(file, "{}", line.replace('\n', "")).unwrap();
                 }
             }
-            Err(e) => println!("{}", e),
+            Err(e) => {
+                error!("OAI response error - {}", e);
+                println!("Bernard, we have a problem!");
+            }
         }
     }
 
